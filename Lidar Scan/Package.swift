0832307ec8bddba6634f0a20db373918c2b20a87
jonathan decorion <jdecorion@jonathans-MacBook-Air.local>
2025-10-28 21:36:47 +0800

    Added Package.swift for Swift Package Manager support

---
+++ Lidar Scan/Package.swift
@@ -0,0 +1,27 @@
+//
+//  Package.swift
+//  
+//
+//  Created by jonathan decorion on 10/28/25.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftUILiDAR",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "SwiftUILiDAR",
+            targets: ["SwiftUILiDAR"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "SwiftUILiDAR",
+            path: "Lidar Scan"
+        )
+    ]
+)
